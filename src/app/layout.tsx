33b6e2e2d9323ad76a8f8a1da147e98aa5c34161
Willians96 <michel.wmoraes@gmail.com>
2026-03-04 19:41:34 -0300

    fix: removendo dynamic com ssr false do Server Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
-import dynamic from "next/dynamic";
-
-const ClientErrorListener = dynamic(() => import("../components/monitor/ClientErrorListener"), { ssr: false });
-
+import ClientErrorListener from "../components/monitor/ClientErrorListener";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
