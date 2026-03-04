8f0fb399ba12f84bab979af51eefcddf8750b9a9
Willians96 <michel.wmoraes@gmail.com>
2026-03-04 20:34:27 -0300

    fix: remove ClientErrorListener do layout para corrigir application error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
-import ClientErrorListener from "../components/monitor/ClientErrorListener";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,10 +29,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <ClientErrorListener />
           {children}
         </body>
       </html>
     </ClerkProvider>
   );
 }
+
